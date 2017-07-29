ddac6f9a521149a5eb4f3a99fb176bcf7109b7f6
Sajaki <Sajaki9@gmail.com>
2017-07-30 00:56:31 +0200

    travis 3.2

---
+++ travis/prepare-epv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# @license GNU General Public License, version 2 (GPL-2.0)
+#
+set -e
+set -x
+
+NOTESTS=$1
+
+if [ "$NOTESTS" == "1" ]
+then
+	cd phpBB
+	composer require phpbb/epv:dev-master --dev --no-interaction
+	cd ../
+fi
